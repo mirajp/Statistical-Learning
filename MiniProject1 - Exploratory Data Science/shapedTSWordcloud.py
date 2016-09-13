6909c211eca489b652efeb1f8646921debc895d1
Patel Miraj <mirage.patel@gmail.com>
2016-09-13 00:50:06 -0400

    Added wordclouds using TS's face as a mask/shape

---
+++ MiniProject1 - Exploratory Data Science/shapedTSWordcloud.py
@@ -0,0 +1,29 @@
+from os import path
+from wordcloud import WordCloud, ImageColorGenerator
+from PIL import Image
+import numpy as np
+
+d = path.dirname(__file__)
+
+# Read the whole text.
+text = open(path.join(d, 'alllyrics.txt')).read()
+
+#ts_mask = Image.open("/afs/ee.cooper.edu/user/p/a/patel6/Statistical-Learning/MiniProject1 - Exploratory Data Science/tscolor.png")
+ts_mask = np.array(Image.open(path.join(d, "ts_mask.png")))
+image_colors = ImageColorGenerator(ts_mask)
+
+# Generate a word cloud image
+wordcloud = WordCloud(background_color="white", mask=ts_mask)
+wordcloud.generate(text)
+wordcloud.to_file(path.join(d, "wordcloud_maskrndcolors.png"))
+
+wordcloud.recolor(color_func=image_colors)
+wordcloud.to_file(path.join(d, "wordcloud_masksrccolors.png"))
+
+# Display the generated image:
+# the matplotlib way:
+import matplotlib.pyplot as plt
+plt.figure()
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.figure()
