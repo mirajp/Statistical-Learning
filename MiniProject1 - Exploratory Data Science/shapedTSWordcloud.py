615ed27c2f852b4316e4347eec8e435ab34085a6
mirajp <mirage.patel@gmail.com>
2016-09-13 23:02:37 -0400

    Minor edits to wordcloud with mask

--- MiniProject1 - Exploratory Data Science/shapedTSWordcloud.py
+++ MiniProject1 - Exploratory Data Science/shapedTSWordcloud.py
@@ -8,7 +8,7 @@ d = path.dirname(__file__)
 # Read the whole text.
 text = open(path.join(d, 'alllyrics.txt')).read()
 
-ts_mask = np.array(Image.open(path.join(d, "ts_mask.png")))
+ts_mask = np.array(Image.open(path.join(d, "ts_mask.2png")))
 image_colors = ImageColorGenerator(ts_mask)
 
 # Generate a word cloud image
@@ -22,7 +22,6 @@ wordcloud.to_file(path.join(d, "wordcloud_masksrccolors.png"))
 # Display the generated image:
 # the matplotlib way:
 import matplotlib.pyplot as plt
-plt.figure()
 plt.imshow(wordcloud)
 plt.axis("off")
-plt.figure()
+plt.show()
