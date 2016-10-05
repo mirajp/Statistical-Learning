7951fc656b70c67490ff5f03cbfde94de0224d8c
Max Howald <maxhowald@gmail.com>
2016-10-05 13:00:36 -0400

    don't calculate total log likelihood for each iteration

--- log-reg/classify.py
+++ log-reg/classify.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 import sys
 import collections
 import math
@@ -10,12 +10,12 @@ NEGATIVE_CLASS = 'B' # Benign
 POSITIVE_CLASS = 'M' # Malignant
 
 # These parameters seemed to work well...
-eta0 = 0.005
-max_iterations = 300
-stopping_val = 60
+eta0 = 0.0001
+max_iterations = 1800
+stopping_val = 1e-12
 # Only set one (or 0) of these to true at a time...
 L1_PENALTY = False
-L2_PENALTY = True
+L2_PENALTY = False
 
 def main():
     if len(sys.argv) != 3:
@@ -63,46 +63,43 @@ def train(X, classes):
     weights = np.random.rand((X.shape[1]), 1)
     diff = stopping_val + 1.
 
-    total_ll = total_log_likelihood(X, classes, weights)
     
     t = 0
     rows = range(len(X))
-    while (diff > stopping_val) and (t < max_iterations):
-        for i in xrange(len(X)):
-            log_prob = 1 / (1 + np.exp(-X[i,:].dot(weights)))
-            error = classes[i] - log_prob
-            error_product = (X[i, :] * error).reshape(X.shape[1],1)
-            weights = weights + eta0 * error_product
+    while (True):
 
-        new_ll = total_log_likelihood(X, classes, weights)
-        diff = np.abs(new_ll - total_ll)
-        total_ll = new_ll
-        
         np.random.shuffle(rows)
         X = X[rows, :]
         new_classes = [None] * len(classes)
         for i in xrange(len(classes)):
             new_classes[i] = classes[rows[i]]
         classes = new_classes
-        t += 1
+
+        for i in xrange(len(X)):
+            prob = 1 / (1 + np.exp(-X[i,:].dot(weights)))
+            error = classes[i] - prob
+            error_product = (X[i, :] * error).reshape(X.shape[1],1)
+            diff = sum(map(np.abs, error_product))
+            if ((diff < stopping_val) or (t > max_iterations)) and t > 3 * len(X):
+                break
+
+            weights[0] += eta0 * error_product[0]
+
+            t += 1
+            if L1_PENALTY:
+                for i in xrange(1, len(weights)):
+                    weights[i] += eta0 * error_product[i] - np.sign(weights[i]) * eta0
+            elif L2_PENALTY:
+                for i in xrange(1, len(weights)):
+                    weights[i] += eta0 * error_product[i] - (weights[i] * eta0)
+            else:
+                for i in xrange(1, len(weights)):
+                    weights[i] += eta0 * error_product[0]
+        if ((diff < stopping_val) or (t > max_iterations)) and  t >  3 * len(X):
+            break
+
     return weights, t
 
-
-def total_log_likelihood(X, Y, W):
-
-    probs = 1 / (1 + np.exp(-X.dot(W)))
-    ones_arr = np.ones((1, probs.shape[1]))
-
-    # epsilon avoid log of negatives or 0...
-    epsilon = 1e-24
-    log_likelihoods = Y * np.log(probs + epsilon) + (ones_arr - Y) * np.log(ones_arr - probs + epsilon)
-    total_ll = -1 * log_likelihoods.sum()
-
-    if L1_PENALTY:
-        total_ll += np.abs(W).sum()
-    if L2_PENALTY:
-        total_ll += np.power(W, 2).sum() / 2
-    return total_ll
 
 
 # calculate the probability and classify as malignant if p > 0.5
