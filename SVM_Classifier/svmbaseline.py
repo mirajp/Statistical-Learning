d88c4970f31c4bfd1cde1b5d5a9279a7324cfbb8
Patel Miraj <mirage.patel@gmail.com>
2016-10-13 18:53:18 -0400

    Included PCA with Gridsearch in the baseline

--- SVM_Classifier/svmbaseline.py
+++ SVM_Classifier/svmbaseline.py
@@ -2,9 +2,11 @@ import csv
 import numpy as np
 from sklearn.svm import SVC
 from sklearn import svm
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
+from sklearn.decomposition import PCA
+
+numPCAComponents = 37
 
 with open("train.csv", "r") as dest_f:
     data_iter = csv.reader(dest_f, 
@@ -32,30 +34,40 @@ Y_train = Y_train.ravel()
 
 
 # Do feature reduction using PCA
-
+# set n_components, chosen to get a sum of variance ratio around 0.95
+# this uses auto for svd solver, (most likely random due to the large # of samples and features)
+pca = PCA(n_components=numPCAComponents)
+pca.fit(X_train)
+print "The top ", numPCAComponents, " components yielded the following variance ratios:\n"
+print(pca.explained_variance_ratio_)
+print "Sum: ", (sum(pca.explained_variance_ratio_))
 
 # Apply the dimensionality reduction to the training set and the test set
+X_train = pca.fit_transform(X_train)
+test = pca.fit_transform(test)
 
 
+Cgridspace = np.linspace(10, 100, num=91, dtype=int)
+Gammagridspace = np.linspace(0.001, 0.5, num=500, dtype=float)
 
+# Only checking radial kernels
+tuned_parameters = [{'kernel': ['rbf'], 'gamma': Gammagridspace,
+                     'C': Cgridspace}]
 
-tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
-                     'C': [1, 10, 100, 1000]},
-                    {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
-#5 fold cross validation
-#clf = GridSearchCV(SVC(C=1), tuned_parameters, cv=5, scoring='precision_macro', n_jobs=4)
-# Swap this out for the previous grid search
-clf = svm.SVC()
+#5 fold cross validation, with 6 jobs/threads
+print "Doing grid search with precision as scoring metric\n"
+clf = GridSearchCV(SVC(C=1), tuned_parameters, cv=5, scoring='precision_macro', n_jobs=6)
 clf.fit(X_train, Y_train)
-#print "Best parameters set found on development set (using precision scoring method):\n"
-#print clf.best_params_
-#Ytest_predictions = clf.predict(X_test)
-#print(classification_report(Y_test, Ytest_predictions))
+
+print "Best parameters set found on development set (using precision scoring method):\n"
+print clf.best_params_
 
 # Get the metric values for the test data
+print "Evaluating metric scores for the test data"
 test_labels = clf.decision_function(test)
 test_ids = np.linspace(1, len(test_labels), num=len(test_labels), dtype=int)
 test_output = np.column_stack((test_ids,test_labels))
 
 #test_output = np.row_stack((["Id", "Prediction"],test_output))
-np.savetxt("testout_withoutpca_withoutgridsearch.csv", test_output, delimiter=",", fmt=("%u", "%.5f"), header="Id, Prediction")
+print "Saving test labels with corresponding ids"
+np.savetxt("testout_withpca_withgridsearch.csv", test_output, delimiter=",", fmt=("%u", "%.5f"), header="Id, Prediction")
